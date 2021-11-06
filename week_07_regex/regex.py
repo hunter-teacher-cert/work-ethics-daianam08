450e4908df9b911aea90d970192e4a6f52f63eb5
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-11-06 02:39:39 +0000

    completed regex :D

--- week_07_regex/regex.py
+++ week_07_regex/regex.py
@@ -1,2 +1,25 @@
-EXTENSION GRANTED :)
-on 11/2/21
+# NOTE from Daiana: Unsure why it prints out with all the spaces and the names separated in this way, but I got the names to print out at least! When I did this in the regex program, it selected the names no problem. Thanks again for the extension!
+
+import re
+
+def find_name(line):
+    pattern = r"((\w+)|(Mr|Ms|Mx)?(\.)?( )(\w)( )?(\w)?( )?(\D)?( )?(\w+))"
+    result = re.findall(pattern,line)
+
+    # print("---")
+
+    # pattern = r"[\w]"
+    # result = result + re.findall(pattern,line)
+
+    return result
+
+
+#result = find_date("10/15/2023 is a October 13, 2025 date as is 1/23/19"
+#print(result)
+
+f = open("namefile.dat")
+for line in f.readlines():
+    #print(line)
+    result = find_name(line)
+    if (len(result)>0):
+        print(result)
